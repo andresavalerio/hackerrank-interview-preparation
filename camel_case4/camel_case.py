f5f135a0ce69b1a65297d87f5b7478853a862bc9
Andresa Valério <andresa.valerio@unifesp.br>
2023-06-13 16:17:37 -0300

    wip: camel case

--- camel_case4/camel_case.py
+++ camel_case4/camel_case.py
@@ -1,7 +1,11 @@
 def combine(words: str, start_capital = False):
     separated_words = words.split(" ")
-    capital_words = separated_words[1:] if not start_capital else separated_words
-    combined_word = separated_words[0] if not start_capital else ''
+    if start_capital:
+        capital_words = separated_words
+        combined_word = ''
+    else:
+        capital_words = separated_words[1:]
+        combined_word = separated_words[0]
 
     for word in capital_words:
         combined_word += word.capitalize()
@@ -26,7 +30,6 @@ def camel_case(s: str):
         # Combine operation
         if str_type == 'M':
             return combine(words)+"()"
-
         elif str_type == 'C':
             return combine(words, start_capital=True)
         else:
